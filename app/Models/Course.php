e7a2acb9476c6f3b6cacc745e265308efdc74ad6
ali dabbagh <alidabbagh.cp@gmail.com>
2020-12-11 16:57:20 +0330

    dar In marhale nabood shodim :{

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -7,5 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Course extends Model
 {
+
     use HasFactory;
 }
--- resources/views/admin/admin_index.blade.php
+++ resources/views/admin/admin_index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.admin')
 
 @section('content')
-   
+
 @endsection
 
 
