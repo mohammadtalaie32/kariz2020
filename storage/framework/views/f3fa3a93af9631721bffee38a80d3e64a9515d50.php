83ecb5b90e05c7e5cd720473fc344caf3df0a4cc
ccc vvv <altalaie@gmail.com>
2021-02-20 22:16:22 -0800

    slideshoe now is fixed

--- storage/framework/views/f3fa3a93af9631721bffee38a80d3e64a9515d50.php
+++ storage/framework/views/f3fa3a93af9631721bffee38a80d3e64a9515d50.php
@@ -1,23 +1,48 @@
 
 <?php $__env->startSection('content'); ?>
 <?php if(count($searched_courses) > 0): ?>
-    <div>
-        <h1 style="text-align:center;color:white">برای ویرایش دوره ها روی نام دوره کلیک کنید</h1>
-    </div>
-    <br>
-    <?php $__currentLoopData = $searched_courses; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $searched_course): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-        <div style = "background-color:rebeccapurple;border: 1px solid black;Border-radius:10px;padding:50px;max-width:100%">
-            <img src="/images/<?php echo e($searched_course->picture); ?>" height="10%" width="10%" alt="NO PHOTO" style="float:left">
-            <a href = "/admin/courses/<?php echo e($searched_course->id); ?>/edit"><h1 style="color:red"> <?php echo e($searched_course->name); ?> </h1></a>
-            <h4 style="color:white"> <?php echo e($searched_course->teacher); ?> </h2>
-            <h4 style="color: white"> <?php echo e($searched_course->date); ?> </h2>
-
-                <p style="color:white;word-wrap:break-word;" class="text"> <?php echo e($searched_course->text); ?>... </p>
-
-
+    <div class="row">
+        <div class="offset-md-1"></div>
+        <div class="col-md-12">
+            <h5 class="alert alert-dismissible alert-info">برای ویرایش دوره ها روی نام دوره کلیک کنید!</h5>
+        </div>
     </div>
 
-    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+
+    <!--Section: Block Content-->
+    <!--Grid row-->
+    <div class="row">
+        <!--Grid column-->
+        <div class="col-lg-12">
+            <!-- Card -->
+            <div class="card wish-list mb-3">
+                <div class="card-body pt-3">
+
+                    <?php $__currentLoopData = $searched_courses; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $searched_course): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                        <div class="row mb-4">
+                            <div class="col-md-5 col-lg-3 col-xl-3">
+                                <div class="view zoom overlay z-depth-1 rounded mb-3 mb-md-0">
+                                    <img style="width 300px;height :150px" class="img-fluid w-100"
+                                         src="/images/<?php echo e($searched_course->picture); ?>" onerror="this.src='<?php echo e(asset('images/no-image.png')); ?>';">
+                                </div>
+                            </div>
+                            <div class="col-md-7 col-lg-9 col-xl-9">
+                                <div class="d-flex justify-content-between">
+                                    <div>
+                                        <h3 style="color:white"><a href="/admin/courses/<?php echo e($searched_course->id); ?>/edit"><?php echo e($searched_course->name); ?></a></h3>
+                                        <p class="mb-1 text-muted text-uppercase small"><?php echo e($searched_course->teacher); ?></p>
+                                        <p class="mb-1 text-muted text-uppercase small"><?php echo e($searched_course->date); ?></p>
+                                        <p class="mb-1 text-muted text-uppercase small"><?php echo e(\Illuminate\Support\Str::limit($searched_course->text, 100, ' ...')); ?></div>
+                                </div>
+                            </div>
+                        </div>
+                        <hr class="mb-4">
+                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+
+                </div>
+                <!--Grid column-->
+            </div>
+            <!--Grid row-->
 <?php else: ?>
     <h2 style="color:white">هیچ دوره ای یا مدرسی با این نام پیدا نشد</h2>
 <?php endif; ?>
