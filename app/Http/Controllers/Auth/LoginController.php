dd0b595eed1dce4b1d35d3b05439454de6332f12
ali dabbagh <alidabbagh.cp@gmail.com>
2021-02-27 22:44:16 +0330

    login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -30,13 +30,13 @@ class LoginController extends Controller
      */
     protected function authenticated(Request $request, $user)
     {
-        $link = ($user->Role)[0]["role_name"];
+
         if ( $user->isAdmin() ) {// do your magic here
 
-            return redirect($link);
+            return redirect("admin");
         }
 
-        return redirect($link);
+        return redirect("/");
     }
 
     /**
